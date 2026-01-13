ad6df4d7cf069dd5e9f4f7ede7e0ad401646c629
Juniper <basil.squared1@gmail.com>
2026-01-13 11:18:40 -0600

    rwgrvgwhoufrw

---
+++ neoforge/src/main/kotlin/SinisterDiscs.kt
@@ -0,0 +1,25 @@
+package basilsquared.sinisterdiscs
+
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.world.item.Item
+import net. neoforged.bus.api.IEventBus
+import net.neoforged.fml.common.Mod
+import net.neoforged.neoforge.registries.DeferredRegister
+
+@Mod("sinister-discs")
+class SinisterDiscs(modEventBus: IEventBus) {
+
+    init {
+        println("SINISTER DISCS: Hello from Kotlin on NeoForge!")
+        ITEMS.register(modEventBus)
+    }
+
+    companion object {
+
+        val ITEMS: DeferredRegister<Item> = DeferredRegister.create(BuiltInRegistries.ITEM, "sinister-discs")
+
+        val DUMMY_ITEM = ITEMS.register("dummy_item") { ->
+            Item(Item.Properties())
+        }
+    }
+}
